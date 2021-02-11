22e23263485e92c70e7e189c7d84c124de0b2147
guipenedo <nostrumg@gmail.com>
2021-02-11 21:28:55 +0100

    removed block_location -> now automatically fetched

--- quiz_stats/quizstats.py
+++ quiz_stats/quizstats.py
@@ -1,31 +1,32 @@
 import json
-from collections import defaultdict, Set
+from collections import defaultdict
 
 import pkg_resources
 import six
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.core.paginator import Paginator
+from edx_user_state_client.interface import XBlockUserState
 from opaque_keys import InvalidKeyError
-from opaque_keys.edx.keys import UsageKey
 from web_fragments.fragment import Fragment
+from webob.response import Response
 from xblock.core import XBlock
-from xblock.fields import Scope, String
+from xblock.fields import Scope
+from xblock.fields import String
 from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioEditableXBlockMixin
-from django.contrib.auth.models import User
-from webob.response import Response
-from django.core.exceptions import PermissionDenied
-from xmodule.modulestore.django import modulestore
-from openedx.core.djangoapps.course_groups.cohorts import get_cohort, is_course_cohorted, get_course_cohorts
-from xmodule.util.sandboxing import get_python_lib_zip
-
 from xmodule.contentstore.django import contentstore
+from xmodule.modulestore.django import modulestore
+from xmodule.util.sandboxing import get_python_lib_zip
 
 from common.djangoapps.student.models import user_by_anonymous_id, get_user_by_username_or_email, CourseEnrollment
 from lms.djangoapps.course_blocks.api import get_course_blocks
-from lms.djangoapps.courseware.user_state_client import DjangoXBlockUserStateClient
+from lms.djangoapps.courseware.models import StudentModule
 from lms.djangoapps.instructor_analytics.basic import list_problem_responses
 from lms.djangoapps.instructor_task.tasks_helper.grades import ProblemResponses
+from openedx.core.djangoapps.course_groups.cohorts import get_cohort, is_course_cohorted, get_course_cohorts
 
 loader = ResourceLoader(__name__)
 
@@ -51,17 +52,12 @@ class QuizStatsXBlock(XBlock, StudioEditableXBlockMixin):
                           scope=Scope.settings,
                           help="Nome do componente na plataforma")
 
-    block_location = String(display_name="block_location",
-                            default="",
-                            scope=Scope.settings,
-                            help="A localização do bloco para o qual queremos mostrar stats. Se vários, separar com vírgula.")
-
     cohort = String(display_name="cohort",
                     default="",
                     scope=Scope.preferences,
                     help="Turma selecionada para todos os editores")
 
-    editable_fields = ('display_name', 'block_location')
+    editable_fields = 'display_name'
     has_author_view = True
 
     # ----------- Views -----------
@@ -69,9 +65,8 @@ class QuizStatsXBlock(XBlock, StudioEditableXBlockMixin):
         return Fragment(
             "<p>Clica em preview ou live para veres o conteúdo deste bloco.</p>"
             "<p>Notas:</p>"
-            "<ol> <li>1. Clicar em edit e colocar a localização do bloco quiz (informação de depuração para a equipa > location). Se vários, separar com vírgula.</li>"
-            "<li>2. Esta unidade (das stats) deverá ter a opção <b>Hide from learners --></b> ativada.</li>"
-            "<li>3. As questões do quiz deverão estar numeradas (começar com 1., 2., etc) para garantir a correta ordenação das stats.</li></ol>")
+            "<ol> <li>1. Esta unidade (das stats) deverá ter a opção <b>Hide from learners --></b> ativada.</li>"
+            "<li>2. As questões do quiz deverão estar numeradas (começar com 1., 2., etc) para garantir a correta ordenação das stats.</li></ol>")
 
     # noinspection PyProtectedMember
     def student_view(self, _context):
@@ -85,7 +80,7 @@ class QuizStatsXBlock(XBlock, StudioEditableXBlockMixin):
         try:
             quizdata = self.get_quiz_data()
         except InvalidKeyError:
-            return Fragment("Erro ao procurar o quiz - corrige a localização nas settings!")
+            return Fragment("Erro ao procurar o quiz - este deverá estar na unidade imediatamente antes!")
         data = {
             'xblock_id': self._get_xblock_loc(),
             'is_course_cohorted': is_course_cohorted(self.course_id),
@@ -108,99 +103,96 @@ class QuizStatsXBlock(XBlock, StudioEditableXBlockMixin):
         return frag
 
     # ----------- Handlers -----------
-    def find_previous_unit_location(self):
-        children = self.get_parent().get_children()
-        for bi in range(len(children) - 1):
-            if children[bi + 1].block_location == self.block_location:
-                return children[bi].block_location
-        return None
-
     def get_quiz_data(self):
         pr_class = ProblemResponses().__class__
         user_id = user_by_anonymous_id(self.xmodule_runtime.anonymous_student_id).id
         course_key = self.course_id
-        usage_keys = [
-            UsageKey.from_string(usage_key_str).map_into_course(course_key)
-            for usage_key_str in self.block_location.split(",")
-        ]
+
+        usage_key = None
+        # sacamos a unidade imediatamente antes desta
+        verts = self.get_parent().get_parent().children
+        for i in range(1, len(verts)):
+            if verts[i].block_id == self.parent.block_id:
+                usage_key = verts[i - 1]
+                break
+        if not usage_key:
+            raise InvalidKeyError
+
         user = get_user_model().objects.get(pk=user_id)
 
         student_data = []
         max_count = settings.FEATURES.get('MAX_PROBLEM_RESPONSES_COUNT')
 
         store = modulestore()
-        user_state_client = DjangoXBlockUserStateClient()
 
         with store.bulk_operations(course_key):
-            for usage_key in usage_keys:
-                course_blocks = get_course_blocks(user, usage_key)
-                for title, path, block_key in pr_class._build_problem_list(course_blocks, usage_key):
-                    # Chapter and sequential blocks are filtered out since they include state
-                    # which isn't useful for this report.
-                    if block_key.block_type in ('sequential', 'chapter'):
+            course_blocks = get_course_blocks(user, usage_key)
+            usernames = set()
+            for title, path, block_key in pr_class._build_problem_list(course_blocks, usage_key):
+                # Chapter and sequential blocks are filtered out since they include state
+                # which isn't useful for this report.
+                if block_key.block_type != "problem":
+                    continue
+
+                block = store.get_item(block_key)
+                generated_report_data = defaultdict(list)
+
+                # Blocks can implement the generate_report_data method to provide their own
+                # human-readable formatting for user state.
+                if hasattr(block, 'generate_report_data'):
+                    try:
+                        user_state_iterator = iter_all_for_block(block_key)
+                        for username, state in self.generate_report_data(block, user_state_iterator, max_count):
+                            generated_report_data[username].append(state)
+                    except NotImplementedError:
+                        pass
+                cohorted = is_course_cohorted(self.course_id)
+
+                def in_cohort(user):
+                    if cohorted:
+                        cohort = get_cohort(user, course_key, assign=False, use_cached=True)
+                        if not cohort or (self.cohort and cohort.name != self.cohort):
+                            # skip this one if not on the requested cohort or has no cohort (instructor)
+                            return False
+                    return True
+
+                def format_name(name):
+                    names = name.split()
+                    if len(names) > 0:
+                        name = names[0]
+                    if len(names) > 1:
+                        name += " " + names[-1]
+                    return name
+
+                responses = []
+                for response in list_problem_responses(course_key, block_key, max_count):
+                    # A block that has a single state per user can contain multiple responses
+                    # within the same state.
+                    try:
+                        user = get_user_by_username_or_email(response['username'])
+                    except User.DoesNotExist:
                         continue
+                    usernames.add(user.username)
+                    if not in_cohort(user):
+                        continue
+                    response['name'] = format_name(user.profile.name)
+                    user_states = generated_report_data.get(response['username'])
+                    response['state'] = json.loads(response['state'])
+                    response['state'].pop('input_state', None)
+                    response['state'].pop('student_answers', None)
 
-                    block = store.get_item(block_key)
-                    generated_report_data = defaultdict(list)
-
-                    # Blocks can implement the generate_report_data method to provide their own
-                    # human-readable formatting for user state.
-                    if hasattr(block, 'generate_report_data'):
-                        try:
-                            user_state_iterator = user_state_client.iter_all_for_block(block_key)
-                            for username, state in self.generate_report_data(block, user_state_iterator, max_count):
-                                generated_report_data[username].append(state)
-                        except NotImplementedError:
-                            pass
-                    cohorted = is_course_cohorted(self.course_id)
-
-                    def in_cohort(user):
-                        if cohorted:
-                            cohort = get_cohort(user, course_key, assign=False, use_cached=True)
-                            if not cohort or (self.cohort and cohort.name != self.cohort):
-                                # skip this one if not on the requested cohort or has no cohort (instructor)
-                                return False
-                        return True
-
-                    responses = []
-                    usernames = set()
-                    for response in list_problem_responses(course_key, block_key, max_count):
-                        # A block that has a single state per user can contain multiple responses
-                        # within the same state.
-                        try:
-                            user = get_user_by_username_or_email(response['username'])
-                        except User.DoesNotExist:
-                            continue
-                        usernames.add(user.username)
-                        if not in_cohort(user):
-                            continue
-                        names = user.profile.name.split()
-                        if len(names) > 0:
-                            response['name'] = names[0]
-                        if len(names) > 1:
-                            response['name'] += " " + names[-1]
-                        user_states = generated_report_data.get(response['username'])
-                        response['state'] = json.loads(response['state'])
-                        response['state'].pop('input_state', None)
-                        response['state'].pop('student_answers', None)
-
-                        if user_states:
-                            response['user_states'] = user_states
-                        responses.append(response)
-                    enrollments = CourseEnrollment.objects.filter(course_id=self.course_id)
-                    print("wtf mate")
-                    print(enrollments)
-                    print(usernames)
-                    for enr in enrollments:
-                        if enr.user.username in usernames:
-                            print(enr.user.username, "is in usernames")
-                            continue
-                        if in_cohort(enr.user):
-                            student_data += [{'username': enr.user.username}]
-                            print(enr.user, "in cohort")
-                        else:
-                            print(enr.user, "not in cohort")
-                    student_data += responses
+                    if user_states:
+                        response['user_states'] = user_states
+                    responses.append(response)
+                enrollments = CourseEnrollment.objects.filter(course_id=self.course_id)
+                for enr in enrollments:
+                    if enr.user.username in usernames:
+                        continue
+                    if in_cohort(enr.user):  # add missing students
+                        student_data.append(
+                            {'username': enr.user.username, 'name': format_name(enr.user.profile.name)})
+                        usernames.add(enr.user.username)
+                student_data += responses
         return student_data
 
     @XBlock.handler
@@ -317,12 +309,12 @@ class QuizStatsXBlock(XBlock, StudioEditableXBlockMixin):
 
                 count += 1
                 report = {
-                    "answer_id": answer_id, # _("Answer ID")
-                    "question": question_text, # _("Question")
-                    "answer": answer_text, # _("Answer")
+                    "answer_id": answer_id,  # _("Answer ID")
+                    "question": question_text,  # _("Question")
+                    "answer": answer_text,  # _("Answer")
                 }
                 if correct_answer_text is not None:
-                    report["correct_answer"] = correct_answer_text # _("Correct Answer")
+                    report["correct_answer"] = correct_answer_text  # _("Correct Answer")
                 yield user_state.username, report
 
 
@@ -332,3 +324,38 @@ def require(assertion):
     """
     if not assertion:
         raise PermissionDenied
+
+
+def iter_all_for_block(block_key, scope=Scope.user_state):
+    """
+    Return an iterator over the data stored in the block (e.g. a problem block).
+
+    You get no ordering guarantees.If you're using this method, you should be running in an
+    async task.
+
+    Arguments:
+        block_key: an XBlock's locator (e.g. :class:`~BlockUsageLocator`)
+        scope (Scope): must be `Scope.user_state`
+
+    Returns:
+        an iterator over all data. Each invocation returns the next :class:`~XBlockUserState`
+            object, which includes the block's contents.
+    """
+    if scope != Scope.user_state:
+        raise ValueError("Only Scope.user_state is supported")
+
+    results = StudentModule.objects.order_by('id').filter(module_state_key=block_key)
+    p = Paginator(results, settings.USER_STATE_BATCH_SIZE)
+
+    for page_number in p.page_range:
+        page = p.page(page_number)
+
+        for sm in page.object_list:
+            state = json.loads(sm.state)
+
+            if state == {}:
+                continue
+            try:
+                yield XBlockUserState(sm.student.username, sm.module_state_key, state, sm.modified, scope)
+            except User.DoesNotExist:
+                pass
